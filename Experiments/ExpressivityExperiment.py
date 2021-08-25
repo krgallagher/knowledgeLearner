dd0a2733b7696a89e9f2ecea1d16ffcff23dfe17
krgallagher <katiegallagher@Katies-MacBook-Pro.local>
2021-08-25 13:51:33 -0500

    refactorings to run multiple examples at once

--- Experiments/ExpressivityExperiment.py
+++ Experiments/ExpressivityExperiment.py
@@ -6,58 +6,61 @@ from SystemPipeline.DatasetPipeline import mainPipeline
 if __name__ == '__main__':
     numberOfExamples = 100
     numShuffles = 5
-    i = 15
-    train = "../en/qa" + str(i) + "_train.txt"
-    test = "../en/qa" + str(i) + "_test.txt"
-    trainingCorpus = bAbIReader(train)
-    testingCorpus = bAbIReader(test)
+    for j in [14]:
+        print("Task:", j)
+        train = "../en/qa" + str(j) + "_train.txt"
+        test = "../en/qa" + str(j) + "_test.txt"
+        trainingCorpus = bAbIReader(train)
+        testingCorpus = bAbIReader(test)
 
-    accuracies = np.empty([numShuffles, 2], dtype=float)
-    parsingTimes = np.empty([numShuffles, 2], dtype=float)
-    learningTimes = np.empty([numShuffles, 2], dtype=float)
+        accuracies = np.empty([numShuffles, 2], dtype=float)
+        parsingTimes = np.empty([numShuffles, 2], dtype=float)
+        learningTimes = np.empty([numShuffles, 2], dtype=float)
 
-    for i in range(0, numShuffles):
-        trainingCorpus.reset()
-        testingCorpus.reset()
-        trainingCorpus.shuffle()
-        accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numberOfExamples,
-                                                           useSupervision=False, useExpressivityChecker=(False, False))
-        accuracies[i][0] = accuracy
-        parsingTimes[i][0] = parsingTime
-        learningTimes[i][0] = learningTime
-        print("\nFluent Representation:\n", "Accuracy: ", accuracy, "\nLearning Time: ", learningTime)
+        for i in range(0, numShuffles):
+            trainingCorpus.reset()
+            testingCorpus.reset()
+            trainingCorpus.shuffle()
+            accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numberOfExamples,
+                                                               useSupervision=False,
+                                                               useExpressivityChecker=(False, False))
+            accuracies[i][0] = accuracy
+            parsingTimes[i][0] = parsingTime
+            learningTimes[i][0] = learningTime
+            print("\nFluent Representation:\n", "Accuracy: ", accuracy, "\nLearning Time: ", learningTime)
 
-        trainingCorpus.reset()
-        testingCorpus.reset()
-        accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numberOfExamples,
-                                                           useSupervision=False, useExpressivityChecker=(False, True))
-        accuracies[i][1] = accuracy
-        parsingTimes[i][1] = parsingTime
-        learningTimes[i][1] = learningTime
-        print("\nEvent Calculus Representation:\n", "Accuracy: ", accuracy, "\nLearning Time: ", learningTime)
+            trainingCorpus.reset()
+            testingCorpus.reset()
+            accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numberOfExamples,
+                                                               useSupervision=False,
+                                                               useExpressivityChecker=(False, True))
+            accuracies[i][1] = accuracy
+            parsingTimes[i][1] = parsingTime
+            learningTimes[i][1] = learningTime
+            print("\nEvent Calculus Representation:\n", "Accuracy: ", accuracy, "\nLearning Time: ", learningTime)
 
-    averageAccuracies = np.average(accuracies, axis=0)
-    accuraciesStandardDeviations = np.std(accuracies, axis=0)
+        averageAccuracies = np.average(accuracies, axis=0)
+        accuraciesStandardDeviations = np.std(accuracies, axis=0)
 
-    averageLearningTimes = np.average(learningTimes, axis=0)
-    learningTimesStandardDeviations = np.std(learningTimes, axis=0)
+        averageLearningTimes = np.average(learningTimes, axis=0)
+        learningTimesStandardDeviations = np.std(learningTimes, axis=0)
 
-    averageParsingTimes = np.average(parsingTimes, axis=0)
-    parsingTimesStandardDeviation = np.std(parsingTimes, axis=0)
+        averageParsingTimes = np.average(parsingTimes, axis=0)
+        parsingTimesStandardDeviation = np.std(parsingTimes, axis=0)
 
-    print("----------------------------------------------------\n")
-    print("Fluent Representation: ")
-    print("Average accuracy: ", averageAccuracies[0])
-    print("Standard deviation: ", accuraciesStandardDeviations[0])
-    print("Average learning time: ", averageLearningTimes[0])
-    print("Standard deviation: ", learningTimesStandardDeviations[0])
-    print("Average parsing time: ", averageParsingTimes[0])
-    print("Standard deviation: ", parsingTimesStandardDeviation[0])
-    print("----------------------------------------------------\n")
-    print("Event Calculus Representation: ")
-    print("Average accuracy: ", averageAccuracies[1])
-    print("Standard deviation: ", accuraciesStandardDeviations[1])
-    print("Average learning time: ", averageLearningTimes[1])
-    print("Standard deviation: ", learningTimesStandardDeviations[1])
-    print("Average parsing time: ", averageParsingTimes[1])
-    print("Standard deviation: ", parsingTimesStandardDeviation[1])
+        print("----------------------------------------------------\n")
+        print("Fluent Representation: ")
+        print("Average accuracy: ", averageAccuracies[0])
+        print("Standard deviation: ", accuraciesStandardDeviations[0])
+        print("Average learning time: ", averageLearningTimes[0])
+        print("Standard deviation: ", learningTimesStandardDeviations[0])
+        print("Average parsing time: ", averageParsingTimes[0])
+        print("Standard deviation: ", parsingTimesStandardDeviation[0])
+        print("----------------------------------------------------\n")
+        print("Event Calculus Representation: ")
+        print("Average accuracy: ", averageAccuracies[1])
+        print("Standard deviation: ", accuraciesStandardDeviations[1])
+        print("Average learning time: ", averageLearningTimes[1])
+        print("Standard deviation: ", learningTimesStandardDeviations[1])
+        print("Average parsing time: ", averageParsingTimes[1])
+        print("Standard deviation: ", parsingTimesStandardDeviation[1])
--- Experiments/minimumExamplesNeeded.py
+++ Experiments/minimumExamplesNeeded.py
@@ -3,28 +3,30 @@ from SystemPipeline.DatasetPipeline import mainPipeline
 import numpy as np
 
 if __name__ == '__main__':
-    trainingSet = "../en/qa" + "16" + "_train.txt"
-    testingSet = "../en/qa" + "16" + "_test.txt"
-    trainingCorpus = bAbIReader(trainingSet)
-    testingCorpus = bAbIReader(testingSet)
-    numShuffles = 5
-    multiplier = 5
-    numExamples = [5, 10, 25, 100]
-    accuracies = np.empty([numShuffles, 5], dtype=float)
-    for i in range(0, numShuffles):
-        trainingCorpus.shuffle()
-        for j in range(0, len(numExamples)):
-            trainingCorpus.reset()
-            testingCorpus.reset()
-            accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numExamples[j])
-            accuracies[i][j] = accuracy
-            print("Shuffle ", i + 1, )
-            print("Number of Examples: ", numExamples[j])
-            print("Accuracy: ", accuracy)
-    averageAccuracies = np.average(accuracies, axis=0)
-    standardDeviations = np.std(accuracies, axis=0)
-    for i in range(0, len(numExamples)):
-        print("Number of Examples: ", numExamples[i])
-        print("Average accuracy: ", averageAccuracies[i])
-        print("Standard deviation: ", standardDeviations[i])
-        print("----------------------------------------------------")
+    toComplete = [9]
+    for task in toComplete:
+        print("TASK: ", task)
+        trainingSet = "../en/qa" + str(task) + "_train.txt"
+        testingSet = "../en/qa" + str(task) + "_test.txt"
+        trainingCorpus = bAbIReader(trainingSet)
+        testingCorpus = bAbIReader(testingSet)
+        numShuffles = 5
+        numExamples = [5, 10, 25, 50, 100, 200]
+        accuracies = np.empty([numShuffles, len(numExamples)], dtype=float)
+        for i in range(0, numShuffles):
+            trainingCorpus.shuffle()
+            for j in range(0, len(numExamples)):
+                trainingCorpus.reset()
+                testingCorpus.reset()
+                accuracy, parsingTime, learningTime = mainPipeline(trainingCorpus, testingCorpus, numExamples[j])
+                accuracies[i][j] = accuracy
+                print("Shuffle ", i + 1, )
+                print("Number of Examples: ", numExamples[j])
+                print("Accuracy: ", accuracy)
+        averageAccuracies = np.average(accuracies, axis=0)
+        standardDeviations = np.std(accuracies, axis=0)
+        for i in range(0, len(numExamples)):
+            print("Number of Examples: ", numExamples[i])
+            print("Average accuracy: ", averageAccuracies[i])
+            print("Standard deviation: ", standardDeviations[i])
+            print("----------------------------------------------------")
